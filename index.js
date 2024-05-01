d40b22ce21d4d5a52341291d39c09ea6a38e24d7
namhai1810 <nnh18102003@gmail.com>
2024-05-01 18:12:16 +0700

    Refactor: Install cors(lỗi chặn api từ be với fe)

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const database = require("./config/database");
 require("dotenv").config();
 const routerApiVer1 = require("./api/v1/routes/index.route");
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 database.connect();
 const port = process.env.PORT;
+
 app.use(bodyParser.json());
+app.use(cors());
 
 // Router V1
 routerApiVer1(app);
