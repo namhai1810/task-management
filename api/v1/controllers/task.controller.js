90d24487f1718bf4bea1c1c474b5345064b013e4
namhai1810 <nnh18102003@gmail.com>
2024-05-01 17:59:54 +0700

    feat: Xóa một hoặc nhiều công việc

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -98,7 +98,23 @@ module.exports.changeMulti = async (req, res) => {
           message: "Cập nhập trạng thái thành công",
         });
         break;
-
+      case "delete":
+        await Task.updateMany(
+          {
+            _id: {
+              $in: ids,
+            },
+          },
+          {
+            deleted: true,
+            deletedAt: Date.now(),
+          }
+        );
+        res.json({
+          code: 200,
+          message: "Xóa thành công",
+        });
+        break;
       default:
         res.json({
           code: 400,
@@ -147,7 +163,7 @@ module.exports.edit = async (req, res) => {
       },
       req.body
     );
-    res.json({
+    res.json({   
       code: 200,
       message: "Cập nhập thành công",
     });
@@ -159,3 +175,24 @@ module.exports.edit = async (req, res) => {
   }
 };
 
+// [DELETE] /api/v1/tasks/delete/:id
+module.exports.delete = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Task.updateOne({
+      _id: id,
+    }, {
+      deleted: true,
+      deletedAt: Date.now(),
+    });
+    res.json({   
+      code: 200,
+      message: "Xóa thành công",
+    });
+  } catch (err) {
+    res.json({
+      code: 400,
+      message: "Tạo mới thất bại",
+    });
+  }
+};
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -10,6 +10,9 @@ router.patch("/change-status/:id", taskController.changeStatus)
 router.patch("/change-multi", taskController.changeMulti)
 router.post("/create", taskController.create)
 router.patch("/edit/:id", taskController.edit)
+router.delete("/delete/:id", taskController.delete)
+
+
 
 
 
