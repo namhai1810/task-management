47a2b99b6254e6c21858f9b86c854b3f8a31a74b
namhai1810 <nnh18102003@gmail.com>
2024-05-01 17:38:19 +0700

    feat: Thêm mới công việc

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -8,5 +8,7 @@ router.get("/",taskController.index)
 router.get("/detail/:id", taskController.detail)
 router.patch("/change-status/:id", taskController.changeStatus)
 router.patch("/change-multi", taskController.changeMulti)
+router.post("/create", taskController.create)
+
 
 module.exports = router;
